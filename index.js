add030adc65b78cc1353fa1cb359bcd6c0bfc42b
Joshua-Salvador <424joshuae.b.s@gmail.com>
2020-10-09 19:48:14 +0800

    For git repo

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const bodyParser =require("body-parser");
+const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
 const https = require("https");
 
+require("dotenv").config();
+
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-
-app.get("/", (req, res) => {  
-  res.render("home", {resultItems: recipes});
+app.get("/", (req, res) => {
+  res.render("home", { resultItems: recipes });
   chunks = [];
   recipes = [];
 });
@@ -26,45 +27,53 @@ app.post("/", (req, res) => {
   let maxCaloriesQuery = req.body.maxCaloriesQuery.toString();
   let diet = req.body.diet;
   let health = req.body.health;
-  console.log(diet)
-  console.log(health)
+  console.log(diet);
+  console.log(health);
   if (minCaloriesQuery === "" && maxCaloriesQuery === "") {
     minCaloriesQuery = "0";
     maxCaloriesQuery = "10000";
   }
-  let url = diet !== "any" ? `https://api.edamam.com/search?q=${query}&app_id=49bb64d6&app_key=690efc729296d5671753022db574e8e4&diet=${diet}&from=0&to=100`: `https://api.edamam.com/search?q=${query}&app_id=49bb64d6&app_key=690efc729296d5671753022db574e8e4&from=0&to=100`;
-  url = health !== "any" ? url + `&health=${health}`: url;
+  let url =
+    diet !== "any"
+      ? `https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}&diet=${diet}&from=0&to=100`
+      : `https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}&from=0&to=100`;
+  url = health !== "any" ? url + `&health=${health}` : url;
   https.get(url, (response) => {
-    response.on("data", (data) => {
-      chunks.push(data);
-    }).on('end', () => {
-      let data = Buffer.concat(chunks);
-      
-      const queryData = data;
-      const jsonQueryData = JSON.parse(queryData); //object is parsed
-      const jsonQueryDataResults = jsonQueryData.hits; //array object, hits
-      //There's something wrong with the parameters for calories
-      jsonQueryDataResults.forEach(element => {
-        if(element.recipe.calories < minCaloriesQuery || element.recipe.calories > maxCaloriesQuery) return
-        recipes.push(element.recipe);
+    response
+      .on("data", (data) => {
+        chunks.push(data);
+      })
+      .on("end", () => {
+        let data = Buffer.concat(chunks);
+
+        const queryData = data;
+        const jsonQueryData = JSON.parse(queryData); //object is parsed
+        const jsonQueryDataResults = jsonQueryData.hits; //array object, hits
+        //There's something wrong with the parameters for calories
+        jsonQueryDataResults.forEach((element) => {
+          if (
+            element.recipe.calories < minCaloriesQuery ||
+            element.recipe.calories > maxCaloriesQuery
+          )
+            return;
+          recipes.push(element.recipe);
+        });
+        // at this point recipes will be complete
+
+        if (recipes[recipes.length - 1] === undefined && searchCount >= 1) {
+          res.render("error", { query: query });
+          chunks = [];
+          recipes = [];
+          labels = [];
+          return;
+        }
+        console.log(res.statusCode);
+        res.redirect("/");
       });
-      // at this point recipes will be complete
-
-      if(recipes[recipes.length - 1] === undefined && searchCount >= 1) {
-        res.render("error", {query:query}); 
-        chunks = [];
-        recipes = [];
-        labels = [];
-        return
-      }
-      console.log(res.statusCode);
-      res.redirect("/");
-    })
-  })
-
+  });
 });
 
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {`Server is running on Port ${PORT}`});
+app.listen(PORT, () => {
+  `Server is running on Port ${PORT}`;
+});
